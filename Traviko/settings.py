18c1b53b9cb7073201f85dbce889d52afee536fa
ayushtamra <tamraayush@gmail.com>
2022-08-16 13:01:13 +0530

    App settings updated
    Signed-off-by: ayushtamra <tamraayush@gmail.com>

--- Traviko/settings.py
+++ Traviko/settings.py
@@ -16,9 +16,9 @@ load_dotenv(find_dotenv())
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
@@ -148,10 +148,14 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # LOGIN_REDIRECT_URL = 'home'
 
+SITE_ID = 1
+
 AUTH_USER_MODEL = "accounts.User"
+
+LOGIN_URL = '/accounts/login'
